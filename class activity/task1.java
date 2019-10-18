290ac18846875d4007b158d59f9898e1cdb86dc2
FatimaYousif <49322171+FatimaYousif@users.noreply.github.com>
2019-10-19 00:30:18 +0500

    task1.java

--- class activity/task1.java
+++ class activity/task1.java
@@ -1,15 +1,21 @@
-
+//Task 1
 class Employee
 {
 int salary;
 int hours;
+//a function to set the values passed as paramaters
+
 public void getInfo(int salary,int hours)
 {
 this.salary=salary;
 this.hours=hours;
 }
+
+//method to work on the salary
+
 public void AddSal()
 {
+
 	if(salary<500)
 	{
 		salary=salary+10;
@@ -18,8 +24,9 @@ public void AddSal()
 public void AddWork()
 {
 	if(hours>6)
-		salary=salary+5;
+		salary=salary+5;  //if hours are greater than 5 the salary gets incremented by 5
 }
+
 			public void display()
 			{
 				System.out.println("FINAL SALARY="+salary+" "+hours);
@@ -31,9 +38,10 @@ class EmployeeEx
 	public static void main(String args[])
 	{
 	Employee e=new Employee();
+        //passing the salary and hours as the arguments
 	e.getInfo(20000,20);
 	e.display();
-	//for less than 500 salary
+	// CHECKING for less than 500 salary
 	e.getInfo(2,20);
 	e.AddSal();
 	e.display();
