c36296b1ddfc41988ce51452ce32484506495564
FatimaYousif <49322171+FatimaYousif@users.noreply.github.com>
2019-10-19 00:35:24 +0500

    task2.java

--- class activity/task2.java
+++ class activity/task2.java
@@ -1,13 +1,21 @@
+//Task 2
+
+//importing util package to use Scanner class
 import java.util.*;
+
 class Area
 {
 double len,brea;
+
+//a setter method to intialize the instance variables
 public void setDim(double len,double brea)
 	
 	{
 	this.len=len;
 	this.brea=brea;
 	}
+
+//this method is to return area in double
 	public double area()
 	{
 	double z=len*brea;
@@ -24,7 +32,10 @@ class AreaEx
 					double l=sc.nextDouble();
 					System.out.println("ENTER BREADTH=");
 					double b=sc.nextDouble();
+
+ //length and breadth taken from the console are passed as arguments to setter method.
 					a.setDim(l,b);
-					System.out.println("AREA=" +a.area());
+
+					System.out.println("AREA=" +a.area()); //the area gets printed in double. 
 			}
 }
