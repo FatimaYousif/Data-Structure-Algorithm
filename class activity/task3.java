95e63bc7edbedf49e48be2a6dc7bfa3a2bad0155
FatimaYousif <49322171+FatimaYousif@users.noreply.github.com>
2019-10-19 00:37:49 +0500

    task3.java

--- class activity/task3.java
+++ class activity/task3.java
@@ -1,12 +1,17 @@
+//task 3
+
 import java.util.*;
 class Area
 {
 double len,brea;
+
+//constructor to initalize the data members
 	Area(double len,double brea)
 	{
 	this.len=len;
 	this.brea=brea;
 	}
+//returnArea method that return area of rectangle in double
 	public double returnArea()
 	{
 	double z=len*brea;
@@ -22,7 +27,7 @@ class AreaEx
 					double l=sc.nextDouble();
 					System.out.println("ENTER BREADTH=");
 					double b=sc.nextDouble();
-					Area a=new Area(l,b);
-					System.out.println("AREA=" +a.returnArea());
+					Area a=new Area(l,b);   //length and breadth taken as i/p are passed as constructor's arguments.
+					System.out.println("AREA=" +a.returnArea());  // area gets printed 
 			}
 }
