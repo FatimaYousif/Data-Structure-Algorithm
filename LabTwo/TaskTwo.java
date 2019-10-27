6023807810546f45f865d6d15c2d3429db0ac670
SameeraAslam <49363175+SameeraAslam@users.noreply.github.com>
2019-10-27 21:56:22 +0500

    Add files via upload

---
+++ LabTwo/TaskTwo.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class TaskTwo {
+    public static void main(String[] args) {
+        int[] array1={1,2,3,4,5}; 
+        int[] array2={1,2,3,4,5}; 
+        System.out.println("ARRAY1 AND ARRAY2 ARE EQUAL"+Arrays.equals(array1, array2)); //returns true if two arrays are equal 
+        
+        
+    }
+    
+}
